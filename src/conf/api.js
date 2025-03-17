5fde41af08d2ffe28b2630a4a3fcd88117c596b6
Purav2003 <shahpurav308@gmail.com>
2025-03-16 21:35:45 -0400

    Update API base URL to new backend endpoint

--- src/conf/api.js
+++ src/conf/api.js
@@ -3,7 +3,7 @@ import { BACKEND_URI } from './env';
 //LOAD ENV VARIABLES
 
 const api = axios.create({
-    baseURL: BACKEND_URI || 'https://wikipedia-backend-94sp.onrender.com/api',
+    baseURL: BACKEND_URI || 'https://wikipedia-backend-1.onrender.com/api',
     headers: {
         'Content-Type': 'application/json',
     },
